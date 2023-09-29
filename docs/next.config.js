837b0af3a0c50ef0c98072ea261f39c40412b053
Matteo La Cognata <matteo.lacognata@gmail.com>
2023-09-29 17:34:11 +0200

    chore(): workflow

--- docs/next.config.js
+++ docs/next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
     output: "export",
     images: { unoptimized: true },
-    basePath: '/air-quality-dashboard/docs',
-    assetPrefix: '/air-quality-dashboard/docs/'
+    basePath: '/air-quality-dashboard',
+    assetPrefix: '/air-quality-dashboard'
 }
 
 module.exports = nextConfig
